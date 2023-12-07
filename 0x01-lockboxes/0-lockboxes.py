2d2f046e8dcd176c2aa4ef313f47fafa640afa33
rodgersxy <rodgersny99@gmail.com>
2023-12-07 06:03:16 +0000

    fixed error

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -13,13 +13,29 @@ def canUnlockAll(boxes):
         Returns true if all boxes can be unlocked. Otherwise false
     """
 
-    keys = {0}
-    for box in range(0, len(boxes)):
-        if box in keys:
-            keys.update(boxes[box])
-            for key in boxes[box]:
-                if key < len(boxes):
-                    keys.update(boxes[key])
-        else:
+    if len(boxes) == 0 or not isinstance(boxes, list):
+        return False
+
+    keys = set(range(len(boxes)))
+    starting_keys = set(boxes[0] or [0])
+    available_keys = starting_keys.copy()
+    used_keys = set([0])
+
+    while True:
+        temp_keys = set()
+
+        for real_index, index in enumerate(available_keys):
+            if isinstance(index, int) and not index > len(keys) - 1:
+                used_keys.add(index)
+
+                for key in boxes[index]:
+                    if key not in used_keys:
+                        temp_keys.add(key)
+
+        if used_keys == keys:
+            return True
+
+        if real_index + 1 == len(available_keys) and len(temp_keys) == 0:
             return False
-    return True
+
+        available_keys = temp_keys
