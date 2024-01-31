c04f285a32e049683ce889a25d6ec8e316e79202
rodgersxy <rodgersny99@gmail.com>
2024-01-31 23:19:09 +0300

    interview quiz

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+been give a pile of coins of different values,
+find the fewest number of coins needed to meet a given amount total
+"""
+
+
+def makeChange(coins, total):
+    """
+    Calculates fewest number of coins needed
+    to meet a given amount total
+    Args:
+        coins: A list of coin values (integers greater than 0).
+        total: The target amount to make change for.
+    """
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[total] if dp[total] != float('inf') else -1
